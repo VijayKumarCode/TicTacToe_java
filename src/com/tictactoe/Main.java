6e5d36b8e7d7ee7176e3437a8536ee9615e6975a
Vijay Kumar <94276052+VijayKumarCode@users.noreply.github.com>
2026-02-06 19:03:48 +0530

    refactor: add and replacing the dummy main with code

--- src/com/tictactoe/Main.java
+++ src/com/tictactoe/Main.java
@@ -1,9 +1,22 @@
+/**
+ * Problem No. #100
+ * Difficulty: Beginner
+ * Description: Application Entry Point
+ * Link: N/A
+ * Time Complexity: N/A
+ * Space Complexity: N/A
+ */
 package com.tictactoe;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.tictactoe.view.MainFrame;
+import javax.swing.SwingUtilities;
+
 public class Main {
     public static void main(String[] args) {
-
+        // Run GUI in the Event Dispatch Thread (Best Practice)
+        SwingUtilities.invokeLater(() -> {
+            MainFrame app = new MainFrame();
+            app.setVisible(true);
+        });
     }
 }
